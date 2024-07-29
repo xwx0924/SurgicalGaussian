b461a8c5dca358de325072d95981ab71da3e93ad
xwx0924 <91236596+xwx0924@users.noreply.github.com>
2024-07-30 02:12:39 +0800

    Update cutting.py

--- arguments/endonerf/cutting.py
+++ arguments/endonerf/cutting.py
@@ -11,8 +11,4 @@ ModelParams = dict(
 OptimizationParams = dict(
     iterations=40_000,
     densify_grad_threshold=0.0003,
-    #warm_up=0,
-    #densify_until_iter=10_000,
-    #lambda_smooth=0.003,  # full=lambda_smooth=0.005  其他0.03
-    #lambda_aiap_cov=200,  # full=100,t=10
 )
