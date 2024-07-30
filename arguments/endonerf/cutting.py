30690c2a6847ac68179317f41c346e8a1e4132bd
xwx <xwxxmu@gmail.com>
2024-07-30 13:45:21 +0800

    默认变更列表

---
+++ arguments/endonerf/cutting.py
@@ -0,0 +1,15 @@
+ModelParams = dict(
+    dataset_type='endonerf',
+    depth_scale=100.0,
+    frame_nums=156,
+    test_id=[1, 9, 17, 25, 33, 41, 49, 57, 65, 73, 81, 89, 97, 105, 113, 121, 129, 137, 145, 153],
+    is_mask=True,
+    depth_initial=True,
+    accurate_mask=False,
+    is_depth=True,
+)
+OptimizationParams = dict(
+    iterations=40_000,
+    lambda_cov=40,
+    lambda_pos=0.2,
+)
